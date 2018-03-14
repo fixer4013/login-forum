5e19971a326887c3f07595cb4de9bb808ac5c25e
Martin <fixer4013@abv.bg>
2018-03-14 13:19:02 +0200

    code modification (proper alignment)

--- app/controllers/withdraw_controller.rb
+++ app/controllers/withdraw_controller.rb
@@ -4,18 +4,16 @@ class WithdrawController < ApplicationController
 	end
 
 	def amount
- 		@user = User.find_by_id(session[:user_id])
-  		@user.saving.cash = @user.saving.cash - params[:amount].to_i
-
-  		if 
-  			@user.saving.cash < params[:amount].to_i
+  		if @user.saving.cash < params[:amount].to_i
   			redirect_to '/protected'
-  		elsif 
-  			params[:amount].to_i < 0
+  		elsif params[:amount].to_i < 0
   			redirect_to '/protected' 
 		else
 			@user.saving.save
 			redirect_to '/withdraw'
 		end
+		@user = User.find_by_id(session[:user_id])
+  		@user.saving.cash = @user.saving.cash - params[:amount].to_i
+  		@user.timetable.save	
 	end
 end
